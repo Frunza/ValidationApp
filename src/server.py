974b147bb2423e84d6caf5674403b4a77d68f51e
Samuel Frunza-Hincu <frunza_samuel@yahoo.com>
2023-02-20 19:45:32 +0100

    added some comments to the testing route of the server

--- src/server.py
+++ src/server.py
@@ -5,15 +5,22 @@ from utils import *
 app = Flask(__name__)
 
 
+# this route is for testing purposes
 @app.route('/')
 def hello():
+    
+    # write a file to test volume usage
     path = os.path.join("/tmp", "testfile")
     with open(path, "w") as file:
         file.write("Test text")
         file.close
+
     result = 'Validation app. Use the validate-yaml endpont to validate that the request body is a valid yaml file.'
+
+    # append a configuration value to the result text to test configmap usage 
     if "VALIDATIONAPP_TEST" in os.environ:
         result = result + ' Value of VALIDATIONAPP_TEST environemnt variable is: ' + os.environ['VALIDATIONAPP_TEST']
+
     return result
 
 @app.route('/validate-yaml', methods=["POST"])
